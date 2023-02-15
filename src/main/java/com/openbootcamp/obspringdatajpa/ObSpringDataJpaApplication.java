2712191ad90b9e61f534a71ae55fd05683242921
fernalp <almanzafernando@hotmail.com>
2023-02-15 11:37:36 -0500

    Se realiza la configuración con MySQL de forma local

--- src/main/java/com/openbootcamp/obspringdatajpa/ObSpringDataJpaApplication.java
+++ src/main/java/com/openbootcamp/obspringdatajpa/ObSpringDataJpaApplication.java
@@ -2,12 +2,31 @@ package com.openbootcamp.obspringdatajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ObSpringDataJpaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ObSpringDataJpaApplication.class, args);
+		ApplicationContext context = SpringApplication.run(ObSpringDataJpaApplication.class, args);
+		CocheRepository repository = context.getBean(CocheRepository.class);
+		System.out.println("Find");
+		System.out.println("El número de datos en base de datos es: " + repository.count());
+		System.out.println("El número de datos en base de datos es: " + repository.count());
+
+		//Crear y almacenar un coche en base de datos
+
+		Coche toyota = new Coche(null,"Toyota","Prius", 2010);
+		repository.save(toyota);
+
+
+		System.out.println("El número de datos en base de datos es: " + repository.count());
+
+		//recuperar todos los coches
+
+		System.out.println(repository.findAll());
+
+
 	}
 
 }
